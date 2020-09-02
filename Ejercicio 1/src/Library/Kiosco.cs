d3e3f28f7a1f16fee9ce0bac6bb1f797904f3149
jeronimo-espiga <jeronimo.espiga@correo.ucu.edu.uy>
2020-09-02 17:55:56 -0300

    tarea 1 corregida

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -6,23 +6,8 @@ namespace Expert_SRP
     {
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) //solo permite vender alfajores, si el dueño quiere vender otra mercaderia tendria que modificar el codigo
         {
-            Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa;
-        }
-        private Double ConvertirAPesos(Double dinero, String moneda)//si el cliente viene con una moneda que no sea dolares o pesos, como euros o yenes, nos encontramos en una razon de cambio
-        {
-            if (moneda.Equals("U$S")) 
-            {
-                return dinero / 30;
-            } 
-            else if (moneda.Equals("$")) 
-            {
-                return dinero;
-            }
-            else 
-            {
-                return -1;
-            }
+            Double pesos = ConversorMoneda.ConvertirAPesos(dinero, moneda);
+            return pesos >= a.CalculoPrecio();
         }
     }
 }
